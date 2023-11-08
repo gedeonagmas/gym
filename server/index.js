c3c9842772ce0e1111af42324995905fb7462829
gedeonagmas <103260131+gedeonagmas@users.noreply.github.com>
2023-11-08 15:35:54 +0300

    Update index.js

--- server/index.js
+++ server/index.js
@@ -14,7 +14,7 @@ const { errorController } = require("./controller/errorController");
 
 app.use(express.json());
 
-app.use(cors({origin: '*'}));
+app.use(cors({origin: 'null'}));
 
 // app.use(cors());
 app.use("/uploads", express.static(path.join(__dirname, "uploads")));
