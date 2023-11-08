feb70cd5ceec5fa540ee4074eead23d3c0adcf8f
gedeonagmas <103260131+gedeonagmas@users.noreply.github.com>
2023-11-08 15:33:07 +0300

    Update index.js

--- server/index.js
+++ server/index.js
@@ -14,25 +14,8 @@ const { errorController } = require("./controller/errorController");
 
 app.use(express.json());
 
-app.use(cors());
-app.use(function (req, res, next) {
+app.use(cors({origin: '*'}));
 
-    // Website you wish to allow to connect
-    res.setHeader('Access-Control-Allow-Origin', 'http://localhost:8888');
-
-    // Request methods you wish to allow
-    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
-
-    // Request headers you wish to allow
-    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
-
-    // Set to true if you need the website to include cookies in the requests sent
-    // to the API (e.g. in case you use sessions)
-    res.setHeader('Access-Control-Allow-Credentials', true);
-
-    // Pass to next layer of middleware
-    next();
-});
 // app.use(cors());
 app.use("/uploads", express.static(path.join(__dirname, "uploads")));
 app.use("/gym/app/v1/", router);
