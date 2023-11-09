3980ef59bee74cb727fbf40cf1111bfad5cf1e0c
gedeonagmas <103260131+gedeonagmas@users.noreply.github.com>
2023-11-09 10:59:34 +0300

    Update index.js

--- server/index.js
+++ server/index.js
@@ -22,7 +22,7 @@ app.use(express.json());
 //   'preflightContinue': false
 // }));
 
-// app.use(cors());
+app.use(cors());
 app.use("/uploads", express.static(path.join(__dirname, "uploads")));
 app.use("/gym/app/v1/", router);
 
